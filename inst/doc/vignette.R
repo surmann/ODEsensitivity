3a22edb8ebee46f917e51058545478f850dcd442
Dirk Surmann <surmann@statistik.tu-dortmund.de>
2015-05-21 13:53:04 +0200

    Initial commit

---
+++ inst/doc/vignette.R
@@ -0,0 +1,9 @@
+### R code from vignette source 'vignette.Rnw'
+### Encoding: ISO8859-1
+
+###################################################
+### code chunk number 1: vignette.Rnw:26-27
+###################################################
+x <- 5
+
+
