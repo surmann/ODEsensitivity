26be8b01cc745c43ab32bb26fd80e4a6dbf6d356
StefanTheers <stefan.theers@tu-dortmund.de>
2015-09-08 17:31:09 +0200

    Morris figures needed in the report

--- files/Package-sensitivity.R
+++ files/Package-sensitivity.R
@@ -18,15 +18,61 @@ library("boot")                                # fuer SA, Sobol
 
 # Test case : the non-monotonic function of Morris
 ## ?morris
-x <- morris(model = morris.fun, factors = 20, r = 4,
-            design = list(type = "oat", levels = 5, grid.jump = 3))
-print(x)
-plot(x)
+set.seed(2015)
+x <- morris(model = morris.fun, factors = 20, r = 10,
+            design = list(type = "oat", levels = 5, grid.jump = 1))
+## Eingabe:
+##   factors      - k, Anzahl Inputs
+##   r            - r, Anzahl Trajektorien
+##   levels       - p, Anzahl Levels
+##   grid.jump    - Hooehe eines Sprungs bei Levels
+
 ## ## Not run:
 ## library(rgl)
 ## plot3d.morris(x) # (requires the package 'rgl')
 ## ## End(Not run)
 
+
+##### Plotting the results for the report ##############################
+set.seed(2015)
+x <- morris(model = sobol.fun, factors = 8, r = 50,
+            design = list(type = "oat", levels = 10, grid.jump = 1))
+print(x)
+plot(x)
+
+mu <- apply(x$ee, 2, mean)
+mu.star <- apply(x$ee, 2, function(x) mean(abs(x)))
+sigma <- apply(x$ee, 2, sd)
+
+
+pdf("C:/Users/theers/ALLE/07_Studium/2012-2015_TU_Dortmund_Statistik_BSc/HiWi-Jobs/LehrstuhlCStat/Bericht/img/sobolfun-morris-mu.pdf",
+    width = 5, height = 5)
+# Plot mu vs. sigma:
+plot(mu, sigma, xlim = c(min(mu), max(mu + 0.1)),
+     xlab = expression(paste(mu)), ylab = expression(sigma))
+text(mu, sigma, labels = paste("x", seq_along(sigma), sep = ""),
+     pos = 4)
+dev.off()
+pdf("C:/Users/theers/ALLE/07_Studium/2012-2015_TU_Dortmund_Statistik_BSc/HiWi-Jobs/LehrstuhlCStat/Bericht/img/sobolfun-morris-mustar.pdf",
+    width = 5, height = 5)
+# Plot mu.star vs. sigma:
+plot(mu.star, sigma, xlim = c(0, max(mu.star + 0.5)),
+     xlab = expression(paste(mu, "*")), ylab = expression(sigma))
+text(mu.star, sigma, labels = paste("x", seq_along(sigma), sep = ""),
+     pos = 4)
+dev.off()
+
+
+##### Scatterplots for Sobol g-function ################################
+pdf("C:/Users/theers/ALLE/07_Studium/2012-2015_TU_Dortmund_Statistik_BSc/HiWi-Jobs/LehrstuhlCStat/Bericht/img/sobolfun-x1.pdf",
+    width = 10, height = 5)
+set.seed(2015)
+n <- 500
+X1 <- data.frame(matrix(runif(8 * n), nrow = n))
+y <- sobol.fun(X1)
+# Scatterplot x1 vs. y:
+plot(X1[, 1], y, xlab = expression(x[1]), ylab = "y")
+dev.off()
 
 ##----------------------------------------------------------------------
 ## sobol(.)
