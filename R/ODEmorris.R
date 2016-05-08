cdb9ce71f160dd20aa85e8ef1fce4b26fc6f54ff
Frank <frank.weber@outlook.de>
2016-05-08 16:18:58 +0200

    Updated documentation for generic functions
    Updated documentation for generic functions (ODEmorris() and
    ODEsobol()).

--- R/ODEmorris.R
+++ R/ODEmorris.R
@@ -1,13 +1,13 @@
 #' @title Morris Screening for ODE Models
 #'
 #' @description
-#'   \code{ODEmorris} is the generic function for sensitivity analysis in ODE 
-#'   models using Morris's elementary effects screening method.
+#'   \code{ODEmorris} is the generic function for performing a sensitivity 
+#'   analysis of ODE models using Morris's elementary effects screening method.
 #'
-#' @param mod 
-#'   either a function with arguments \code{Time, State, Pars} (for 
-#'   \code{\link{ODEmorris.default}} or an object of class \code{ODEnetwork} 
-#'   (for \code{\link{ODEmorris.ODEnetwork}}).
+#' @param mod
+#'   either a model function supplied in the manner as needed for 
+#'   \code{\link[deSolve]{ode}} (for \code{\link{ODEmorris.default}}) or an 
+#'   object of class \code{ODEnetwork} (for \code{\link{ODEmorris.ODEnetwork}}).
 #' @param ...
 #'   further arguments passed to methods, see \code{\link{ODEmorris.default}} 
 #'   and \code{\link{ODEmorris.ODEnetwork}}.
@@ -16,10 +16,11 @@
 #'   There are two methods for this generic function: 
 #'   \code{\link{ODEmorris.default}} (for general ODE models) and
 #'   \code{\link{ODEmorris.ODEnetwork}} (for objects of class \code{ODEnetwork},
-#'   see the package \code{ODEnetwork}).
+#'   see package \code{ODEnetwork}).
 #'
 #' @author Frank Weber
 #' @seealso \code{\link{ODEmorris.default}, \link{ODEmorris.ODEnetwork}}
+#' 
 #' @export
 #'
 
--- R/ODEsobol.R
+++ R/ODEsobol.R
@@ -1,13 +1,13 @@
-#' @title Sobol' SA for ODE Models
+#' @title Sobol' Sensitivity Analysis for ODE Models
 #'
 #' @description
-#' \code{ODEsobol} is the generic function for Sobol' sensitivity analysis in
-#' ODE models.
+#' \code{ODEsobol} is the generic function for performing a Sobol' sensitivity 
+#' analysis of ODE models.
 #'
-#' @param mod 
-#'   either a function with arguments \code{Time, State, Pars} (for 
-#'   \code{\link{ODEsobol.default}} or an object of class \code{ODEnetwork} (for
-#'   \code{\link{ODEsobol.ODEnetwork}}.
+#' @param mod
+#'   either a model function supplied in the manner as needed for 
+#'   \code{\link[deSolve]{ode}} (for \code{\link{ODEsobol.default}}) or an 
+#'   object of class \code{ODEnetwork} (for \code{\link{ODEsobol.ODEnetwork}}).
 #' @param ...
 #'   further arguments passed to methods, see \code{\link{ODEsobol.default}} and
 #'   \code{\link{ODEsobol.ODEnetwork}}.
@@ -16,7 +16,7 @@
 #'   There are two methods for this generic function: 
 #'   \code{\link{ODEsobol.default}} (for general ODE models) and
 #'   \code{\link{ODEsobol.ODEnetwork}} (for objects of class \code{ODEnetwork},
-#'   see the package \code{ODEnetwork}).
+#'   see package \code{ODEnetwork}).
 #'
 #' @author Frank Weber
 #' @seealso \code{\link{ODEsobol.default}, \link{ODEsobol.ODEnetwork}}
