40a5dd4d9787681d6364fd432aa512106fcaa4f8
StefanTheers <stefan.theers@tu-dortmund.de>
2018-05-23 11:43:12 +0200

    tdcc() example documentation

--- R/tdcc.R
+++ R/tdcc.R
@@ -27,9 +27,11 @@
 #' }
 #'
 #' @examples
-#' ranking <- rbind(1:20,
-#'                  c(1,3,2,4,16,10,19,12,18,17,
-#'                    20,5,14,7,8,11,6,15,9,13))
+#' # b=2 sensitivity analysis techniques A and B that rate the influence of 
+#' # k=20 variables/ input parameters (example taken from Iman and Conover, 1987):
+#' ranking <- rbind(A = 1:20,
+#'                  B = c(1,3,2,4,16,10,19,12,18,17,
+#'                        20,5,14,7,8,11,6,15,9,13))
 #' tdcc(ranking, pearson = TRUE, plot = TRUE)
 #'
 #' @author Stefan Theers
